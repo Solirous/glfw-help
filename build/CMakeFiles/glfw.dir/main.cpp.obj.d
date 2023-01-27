31820026f91ac04019d5bafe9f7b6c92ac962837
unknown <dogethanthanh@gmail.com>
2023-01-27 19:16:39 +0700

    need help linking error

---
+++ build/CMakeFiles/glfw.dir/main.cpp.obj.d
@@ -0,0 +1,2 @@
+CMakeFiles/glfw.dir/main.cpp.obj: \
+ C:\Users\Admin\Desktop\code\glfw\src\main.cpp
---
+++ build/CMakeFiles/nerd.dir/main.cpp.obj.d
@@ -0,0 +1,8 @@
+CMakeFiles/nerd.dir/main.cpp.obj: \
+ C:\Users\Admin\Desktop\code\glfw\src\main.cpp \
+ c:\users\admin\desktop\code\glfw\external\glfw\include\glfw3.h \
+ c:\mingw\lib\gcc\mingw32\6.3.0\include\stddef.h \
+ c:\mingw\lib\gcc\mingw32\6.3.0\include\stdint.h \
+ c:\mingw\include\stdint.h c:\mingw\include\_mingw.h \
+ c:\mingw\include\msvcrtver.h c:\mingw\include\w32api.h \
+ c:\mingw\include\sdkddkver.h c:\mingw\include\gl\gl.h
